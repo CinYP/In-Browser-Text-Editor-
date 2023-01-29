937abc679726e9f5dfa6f5dc7c64dff7dc4c17eb
CinYP <111353883+CinYP@users.noreply.github.com>
2023-01-29 10:46:06 -0500

    added hot module

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,7 +13,14 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {console.error('putDb not implemented');
+
+//const db = await openDB();
+
+// const result = await request;
+//   console.log('🚀 - data saved to the database', result);
+
+};
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => console.error('getDb not implemented');
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,6 +18,10 @@ module.exports = () => {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
+    devServer: {
+      // The `hot` option is to use the webpack-dev-server in combination with the hot module replacement API.
+      hot: 'only',
+    },
     plugins: [
       new HtmlWebpackPlugin({
         template: './index.html',
