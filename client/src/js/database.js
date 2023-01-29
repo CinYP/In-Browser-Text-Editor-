eb8f047f79e4015540a54c06f545b749045e5500
CinYP <111353883+CinYP@users.noreply.github.com>
2023-01-29 12:55:44 -0500

    putDb in database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -14,15 +14,17 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {console.error('putDb not implemented');
-
-const db = await openDB();
-
-const result = await request;
+  // Create a connection to the database database and version we want to use.
+  console.log('PUT to the database');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.put({ id: id, todo: content });
+  const result = await request;
   console.log('🚀 - data saved to the database', result);
-
 };
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
-
+export const getDb = async () => {console.error('getDb not implemented');
+}
 initdb();
--- client/src/js/install.js
+++ client/src/js/install.js
@@ -25,5 +25,5 @@ butInstall.addEventListener('click', async () => {
 
 // TODO: Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
-    window.deferredPrompt = null;
+    window.appinstalled = null;
 });
