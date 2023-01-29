b498aa55ec3bb2106ea1908f658ce0f84df04a0f
CinYP <111353883+CinYP@users.noreply.github.com>
2023-01-29 13:13:33 -0500

    database.js complete

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -26,5 +26,14 @@ export const putDb = async (content) => {console.error('putDb not implemented');
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {console.error('getDb not implemented');
+console.log('GET from the database');
+const jateDb = await jateDB('jate', 1);
+const tx = jateDb.transaction('jate', 'readonly');
+const store = tx.objectStore('jate');
+const request = store.getAll();
+const result = await request;
+console.log('result.value', result);
+return result;
 }
+
 initdb();
